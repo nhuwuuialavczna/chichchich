2fc65f0551f3f564cd6d07fd389abc6476efe214
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-02-24 13:38:44 +0700

    vzvxvzxv

--- public/xuly.js
+++ public/xuly.js
@@ -1,4 +1,5 @@
-var socket = io("http://localhost:3000");
+// var socket = io("http://localhost:3000");
+var socket = io("https://chichchich.herokuapp.com/");
 
 socket.on("server-send-dki-thatbai", function(){
   alert("Sai Username (co nguoi da dang ki roi!!!)");
