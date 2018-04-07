a06cfd20060056e8d8812003ef53e39454f84978
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-04-07 20:37:57 +0700

    Đã hoàn thành khá ổn định. Cả chức năng thông báo. Lẫn thông báo âm thanh

--- app.js
+++ app.js
@@ -27,7 +27,32 @@ var postRouter = require('./routes/post');
 
 app.use('/', indexRouter);
 app.use('/post', postRouter);
+app.use(function(req, res, next) {
+    var oneof = false;
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-method']) {
+        res.header('Access-Control-Allow-Methods', req.headers['access-control-request-method']);
+        oneof = true;
+    }
+    if(req.headers['access-control-request-headers']) {
+        res.header('Access-Control-Allow-Headers', req.headers['access-control-request-headers']);
+        oneof = true;
+    }
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
 
+    // intercept OPTIONS method
+    if (oneof && req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
 
 app.get('/message', function (req, res) {
     // Don't bother about this :)
