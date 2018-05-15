af510d31e4d2b6d64ae0c7163bc2402a10ec98ac
vegito165 <15130052@st.hcmuaf.edu.vn>
2018-05-16 02:19:43 +0700

    Đã hoàn thành khá ổn định. Cả cvchức năng thông báo. Lẫn thông báo âm thanh

--- model/account.sql
+++ model/account.sql
@@ -1,17 +1,18 @@
 /*
-Navicat PGSQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : hau
-Source Server Version : 100300
-Source Host           : ec2-54-204-21-226.compute-1.amazonaws.com:5432
-Source Database       : df07n687imb2qc
-Source Schema         : public
+ Source Server         : hau
+ Source Server Type    : PostgreSQL
+ Source Server Version : 100003
+ Source Host           : ec2-54-204-21-226.compute-1.amazonaws.com:5432
+ Source Catalog        : df07n687imb2qc
+ Source Schema         : public
 
-Target Server Type    : PGSQL
-Target Server Version : 100300
-File Encoding         : 65001
+ Target Server Type    : PostgreSQL
+ Target Server Version : 100003
+ File Encoding         : 65001
 
-Date: 2018-04-10 05:50:29
+ Date: 11/05/2018 23:02:45
 */
 
 
@@ -20,18 +21,21 @@ Date: 2018-04-10 05:50:29
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."account";
 CREATE TABLE "public"."account" (
-"email" varchar(255) COLLATE "default" NOT NULL,
-"password" varchar(255) COLLATE "default",
-"name" varchar(255) COLLATE "default",
-"ip" varchar(255) COLLATE "default",
-"hinhanh" varchar(255) COLLATE "default",
-"banbe" varchar(4000) COLLATE "default",
-"baiviet" varchar(4000) COLLATE "default",
-"filedaup" varchar(4000) COLLATE "default",
-"danhgia" int8
+  "email" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "password" varchar(255) COLLATE "pg_catalog"."default",
+  "name" varchar(255) COLLATE "pg_catalog"."default",
+  "ip" varchar(255) COLLATE "pg_catalog"."default",
+  "hinhanh" varchar(255) COLLATE "pg_catalog"."default",
+  "banbe" varchar(4000) COLLATE "pg_catalog"."default",
+  "baiviet" varchar(4000) COLLATE "pg_catalog"."default",
+  "filedaup" varchar(4000) COLLATE "pg_catalog"."default",
+  "danhgia" int8,
+  "diem" int8 DEFAULT 0,
+  "caudalam" varchar(4000) COLLATE "pg_catalog"."default",
+  "caudathem" varchar(4000) COLLATE "pg_catalog"."default",
+  "thongbao" varchar(4000) COLLATE "pg_catalog"."default",
+  "nguoixembaiviet" varchar(255) COLLATE "pg_catalog"."default"
 )
-WITH (OIDS=FALSE)
-
 ;
 COMMENT ON COLUMN "public"."account"."email" IS 'tài khoản';
 COMMENT ON COLUMN "public"."account"."password" IS 'mật khẩu';
@@ -42,25 +46,23 @@ COMMENT ON COLUMN "public"."account"."banbe" IS 'Danh sách bạn bè';
 COMMENT ON COLUMN "public"."account"."baiviet" IS 'Danh sách bài biết';
 COMMENT ON COLUMN "public"."account"."filedaup" IS 'Các file đã up lên web. Chỉ lưu đường dẫn';
 COMMENT ON COLUMN "public"."account"."danhgia" IS 'Đánh giá của người khác về người này';
+COMMENT ON COLUMN "public"."account"."diem" IS 'Điểm của user';
+COMMENT ON COLUMN "public"."account"."caudalam" IS 'Câu hỏi user đã làm';
+COMMENT ON COLUMN "public"."account"."caudathem" IS 'Câu hỏi user đã thêm';
+COMMENT ON COLUMN "public"."account"."thongbao" IS 'Thông báo cho user nào đó';
+COMMENT ON COLUMN "public"."account"."nguoixembaiviet" IS 'Người xem bài viết. Bao gồm. 1, friend, all';
 
 -- ----------------------------
 -- Records of account
 -- ----------------------------
-INSERT INTO "public"."account" VALUES ('15130052@st.hcmua', '111111', '15130052@st.hcmuaf.edu.vn', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523267334854_393c4776ca8deb35a2392c2e33e09a0d.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('aaa@gmail.com', '111111', 'hậu', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523271309711_393c4776ca8deb35a2392c2e33e09a0d.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('aa@gmail.com', '111111', 'hậu', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523271270872_15439818_254731788278338_8656453826238181144_n.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('admin@gmai.com', '123456', 'Lực', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523294620183_29790619_2092967871027667_7710514210557373661_n.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('gggg@gmail.com', '111111', 'vvvv', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523270355125_13178547_102524736832378_1861582596355730142_n.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('nguhoc@gmail.com', '147123', 'zxvzxcvzx', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523274858279_Chrysanthemum.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('vvvv@gmail.com', '111111', 'Tân', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523271390845_15732667_266534673764716_2240923064537937691_o.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('zxcxzv@gmai.com', '122222', 'haaaju', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523292976007_29790619_2092967871027667_7710514210557373661_n.jpg', '', '', '', '0');
-INSERT INTO "public"."account" VALUES ('zzzzzz@gmail.com', 'zzzzzz', 'hhss', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=imgUploader_1523279183518_Koala.jpg', '', '', '', '0');
-
--- ----------------------------
--- Alter Sequences Owned By 
--- ----------------------------
+INSERT INTO "public"."account" VALUES ('VanHuynh', '123456', 'Huỳnh', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=1525415233345_img.jpg', 'tanhau', '', '', 0, 0, NULL, NULL, 'Không có thông báo', 'all');
+INSERT INTO "public"."account" VALUES ('hiendt', 'hiendt', 'hiendt', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=1525671782651_WIN_20180417_23_58_08_Pro.jpg', '', '', '', 0, 0, NULL, NULL, NULL, NULL);
+INSERT INTO "public"."account" VALUES ('Thinh', '123456', 'THỊNH', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=1525613525437_IMG20180304113754.jpg', '', '', '', 0, 0, NULL, NULL, 'Web bị ngu rôi.', NULL);
+INSERT INTO "public"."account" VALUES ('admin', '123456', 'Tấn Hậu', 'hau', 'https://uploadserver.azurewebsites.net/img?fileName=1525583611444_27983201_336839080145469_5194809768923833291_o.jpg', 'admin0107', '', '', 0, 0, NULL, NULL, 'Web bị ngu rồi', 'friend');
+INSERT INTO "public"."account" VALUES ('admin0107', 'admin', 'admin', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=1525583977380_roi-luong-tu-3.jpg', '', '', '', 0, 0, NULL, NULL, NULL, NULL);
+INSERT INTO "public"."account" VALUES ('anhdeptrai', '123456', 'Anh Đẹp Trai', '127.0.0.1', 'https://uploadserver.azurewebsites.net/img?fileName=1525607758549_Cap.PNG', '', '', '', 0, 0, NULL, NULL, NULL, 'all');
 
 -- ----------------------------
 -- Primary Key structure for table account
 -- ----------------------------
-ALTER TABLE "public"."account" ADD PRIMARY KEY ("email");
+ALTER TABLE "public"."account" ADD CONSTRAINT "account_pkey" PRIMARY KEY ("email");
